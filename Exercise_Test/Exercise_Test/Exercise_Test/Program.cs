ccfebc2520ade3a65d2646fc8503be4d8b743c8d
xo0821ms <49581796+xo0821ms@users.noreply.github.com>
2019-04-19 02:04:17 +0800

    Add files via upload

---
+++ Exercise_Test/Exercise_Test/Exercise_Test/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AAA b = new BBB();
+            b.CW();
+            Test(ref b);
+            b.CW();
+        }
+
+        private static void Test(ref AAA a)
+        {
+            a = new CCC();
+        }
+    }
+
+
+    public class AAA
+    {
+        public virtual void CW()
+        {
+            Console.WriteLine("aaa");
+        }
+    }
+
+    class BBB : AAA
+    {
+        public override void CW()
+        {
+            Console.WriteLine("bbb");
+        }
+    }
+
+    class CCC : AAA
+    {
+        public override void CW()
+        {
+            Console.WriteLine("ccc");
+        }
+    }
+
+}
