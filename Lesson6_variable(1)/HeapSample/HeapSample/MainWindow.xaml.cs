86b3f257e01b83bc966a041f29dd6be70fa4bf7b
xo0821ms <49581796+xo0821ms@users.noreply.github.com>
2019-04-21 02:31:12 +0800

    Add files via upload

---
+++ Lesson6_variable(1)/HeapSample/HeapSample/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HeapSample
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Window> winList;
+        private void Buttom1_Click(object sender, RoutedEventArgs e)
+        {
+            winList = new List<Window>();
+            for (int i = 0; i < 15000; i++)
+            {
+                Window s = new Window();
+                winList.Add(s);
+            }
+        }
+
+        private void Buttom2_Click(object sender, RoutedEventArgs e)
+        {
+            winList.Clear();
+        }
+    }
+}
---
+++ Lesson6_variable(1)/variable_Stack_Heap/variable_Stack_Heap/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace variable_Stack_Heap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SSS();   
+        
+
+        }
+
+
+        static void SSS() {
+            double res = 3.0 / 4.0;// 3.0(double)/4.0(double) = 0.75(double)
+            double result = 3 / 4;//  4(int)/3(int) = 0(int)然後才把0放到result(double)裡
+            Console.WriteLine(res);
+            Console.WriteLine(result);
+        }
+
+        
+
+       static void GetMyType()//得到具體類型方式
+        {
+            Type myType = typeof(Form);
+            Console.WriteLine(myType.BaseType.BaseType.FullName);
+
+            PropertyInfo[] pInfos =  myType.GetProperties();
+            foreach (var v in pInfos)
+            {
+                Console.WriteLine(v.Name);
+            }
+            Console.WriteLine(".....................................................");
+            MethodInfo[] mInfos = myType.GetMethods();
+            foreach (var v in mInfos)
+            {
+                Console.WriteLine(v.Name);
+            }
+
+        }
+
+       static void Class_Dynamic()//Dynamic練習
+        {
+            dynamic myVar = 100;
+            Console.WriteLine(myVar);
+            myVar = "hahaha";
+            Console.WriteLine(myVar);
+        }
+
+    }
+
+
+    
+
+
+}
